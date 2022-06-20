7e9e1135a2ea1bc3e37ad68fda3e79fba8074087
KIM HAJIN <rlagkwls629@gmail.com>
2022-06-20 17:45:34 +0900

    time_attack

---
+++ hajin/hajin/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for hajin project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hajin.settings')
+
+application = get_wsgi_application()
