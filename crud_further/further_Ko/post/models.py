3ac7bd92905ff979f6fc3c3444ad64553529d424
khw7876 <skanahrwi@naver.com>
2022-06-22 14:20:49 +0900

    8. serializer를 통해서 나와 같은 태그를 가진 유저 검색 구현

--- crud_further/further_Ko/post/models.py
+++ crud_further/further_Ko/post/models.py
@@ -11,6 +11,8 @@ class Tag(models.Model):
         return f"{self.name}"
 
 class PostModel(models.Model):
+    class Meta:
+        db_table = "postmodel"
     author = models.ForeignKey("user.User", on_delete=models.CASCADE)
     contents = models.TextField("내용", max_length=200)
     tag = models.ManyToManyField(Tag, verbose_name="태그")
--- crud_further/further_Ko/user/serializers.py
+++ crud_further/further_Ko/user/serializers.py
@@ -1,14 +1,25 @@
+from dataclasses import field
 from rest_framework import serializers
 
 from post.models import PostModel
+from post.models import Tag as TagModel
 from .models import User as UserModel
 
+class TagSerializer(serializers.ModelSerializer):
+    same_post = serializers.SerializerMethodField()
 
+    def get_same_post(self,obj):
+        return [{"same_username" : obj.author.username, "same_post_content" : obj.contents} for obj in obj.postmodel_set.all()]
+        
+    class Meta:
+        model = TagModel
+        fields = ["name", "same_post"]
 
 class PostSerializer(serializers.ModelSerializer):
+    tag = TagSerializer(many = True)
     class Meta:
         model = PostModel
-        fields = ["contents", 'id']
+        fields = ["contents", 'id', 'tag']
 
 class UserSerializer(serializers.ModelSerializer):
     posts = PostSerializer(many=True, source="postmodel_set")
