7989655ba9bad27274dcd5e11bf1fc7719d486e0
khw7876 <skanahrwi@naver.com>
2022-06-22 13:44:35 +0900

    1. post앱에 tag 구현

--- crud_further/further_Ko/post/models.py
+++ crud_further/further_Ko/post/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Tag(models.Model):
+    class Meta:
+        db_table = "tag"
+    name = models.CharField("태그이름", max_length=20)
+
+    def __str__(self):
+        return f"{self.name}"
+
 class PostModel(models.Model):
     author = models.ForeignKey("user.User", on_delete=models.CASCADE)
     contents = models.TextField("내용", max_length=200)
+    tag = models.ManyToManyField(Tag, verbose_name="태그")
 
     def __str__(self):
         return f"{self.author}"
